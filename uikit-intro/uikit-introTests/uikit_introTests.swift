a7e513fd4b8eb00cf5bd936fcbd0d17d2553094d
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-07-15 13:00:30 +0530

    feat: uikit intro

---
+++ uikit-intro/uikit-introTests/uikit_introTests.swift
@@ -0,0 +1,17 @@
+//
+//  uikit_introTests.swift
+//  uikit-introTests
+//
+//  Created by Prince on 15/07/25.
+//
+
+import Testing
+@testable import uikit_intro
+
+struct uikit_introTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
