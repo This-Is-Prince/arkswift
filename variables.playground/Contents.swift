9f22eea08702d32f57bbb58dae8b0a01e4dd0a30
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-07-15 12:31:15 +0530

    feat: variables playground

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -40,3 +40,53 @@ let devices: [String: String] = [
 
 print(devices)
 print(devices["laptop"])
+
+
+func printInstructiorsName(a: String, b: Int, c: Float, d: Bool) {
+    print(a, b, c, d)
+}
+
+printInstructiorsName(a: "1", b: 1, c: 2.0, d: false)
+
+func add(a firstNumber: Int, b secondNumber: Int ) -> Int {
+    return firstNumber + secondNumber
+}
+
+print(add(a: 5, b: 6))
+
+let programmingLanguage = "Swift"
+var currentVersion = 5
+currentVersion = 6
+
+let language = "Swift"
+var progress: Double = 15.5
+let message = "My progress in \(language) is \(progress)%."
+
+print(message)
+
+var highScores = [100, 95, 78]
+
+print(highScores)
+highScores.append(89)
+print(highScores)
+print(highScores[2], highScores.max(), highScores.count, highScores.min(), highScores.capacity)
+
+var dict = [
+    1: "One",
+    2: "Two",
+]
+print(dict)
+dict[3] = "Three"
+print(dict)
+
+var dict2: [Int: Any] = [
+    1: "One",
+    2: 2,
+    3: "Three False"
+]
+
+if let one = dict2[2] as? Int, one == 2 {
+    print("Present")
+} else {
+    print("None")
+}
