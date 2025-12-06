256ae61c6e59a771f8d960cdc629d033004781c8
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-12-06 16:08:49 +0530

    feat: concurrency

---
+++ swift_docs/concurrency.swift
@@ -0,0 +1,17 @@
+
+func fetchUserID(from server: String) async -> Int {
+    if server == "primary" {
+        return 97
+    }
+    
+    return 501
+}
+
+func fetchUsername(from server: String) async -> String {
+    let userID = await fetchUserID(from: server)
+    if userID == 501 {
+        return "John Doe"
+    }
+    
+    return "Guest"
+}
